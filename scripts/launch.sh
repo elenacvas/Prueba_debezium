69031f851d6d210bdda6c8faa394ba241916006c
elena.cuevas <elena.cuevas@masmovil.com>
2021-02-16 18:57:29 +0100

    update

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,7 +4,7 @@ $ORACLE_HOME/bin/sqlplus sys/oracle as sysdba @/app/setup_database.sql
 
 ## Importing apollo_prop tables
 echo "Importing APOLLO_PROP tables"
-$ORACLE_HOME/bin/imp apollo_prop/apollo constraints=N grants=N indexes=N full=Y file=/app/exp_apollo.dmp 
+$ORACLE_HOME/bin/imp apollo_prop/apollo constraints=N grants=N indexes=N full=Y file=/app/exp_packgetype.dmp 
 
 echo "Change apollo_prop table logging"
 $ORACLE_HOME/bin/sqlplus sys/oracle as sysdba @/app/apollo_prop_logging.sql
